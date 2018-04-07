2e5240c95d1affef07aeafceda48875725711dcd
Vadim Demid <demid.by@gmail.com>
2018-04-07 20:32:27 +0300

    jar started!!!

--- src/main/java/by/fanip/testic/TestLambda.java
+++ src/main/java/by/fanip/testic/TestLambda.java
@@ -1,8 +1,9 @@
 package by.fanip.testic;
 
 public class TestLambda {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.out.println("Hello");
+        Thread.sleep(3000);
     }
 }
--- src/main/java/by/fanip/testic/TestStream.java
+++ src/main/java/by/fanip/testic/TestStream.java
@@ -6,14 +6,16 @@ import java.nio.file.Paths;
 
 public class TestStream {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         int[] count = new int[]{1};
-        Files.lines(Paths.get("D:\\java\\project\\test\\src\\test01\\2015.txt"))
+        Files.lines(Paths.get("D:\\java\\project\\test\\src\\main\\resourses\\2015.txt"))
                 .filter((s) -> s.contains("малыш"))
                 .sorted(((o1, o2) -> o2.compareTo(o1)))
                 .peek((s) -> System.out.println(count[0]++ + " " + s))
                 .limit(50)
                 .forEach(System.out::println);
+
+        Thread.sleep(5000);
 
     }
 }
